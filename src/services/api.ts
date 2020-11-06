57e8eede4248bdecfe45dfb45245d2d36e7387d8
Lucas Erthal <57104379+LucasErthal@users.noreply.github.com>
2020-11-06 01:37:43 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:'http://192.168.0.10:3333' //'https://tas-deploy.herokuapp.com/'
+  baseURL:'https://tas-deploy.herokuapp.com/' //'http://192.168.0.10:3333'
 })
 
 export default api;
